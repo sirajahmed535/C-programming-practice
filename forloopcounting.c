7b08f859491775955f9a8f8256f261ad17c04452
siraj ahmed <sa5985117@gmail.com>
2020-12-09 09:29:48 +0500

    Add files via upload

---
+++ forloopcounting.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//for loop printf numbers from 0 to 10
+
+int main()
+{
+	int i;
+	printf("printf the numbers from 1 to 10:\n\n");
+	for(i=1;i<=10;i++)
+	{
+		printf("\t%d\n",i);
+	}
+	return 0;
+}
