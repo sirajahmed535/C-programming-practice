f7cb0f90f3c18063731f849233539a11aa5fe67f
siraj ahmed <sa5985117@gmail.com>
2020-12-09 10:29:30 +0500

    Add files via upload

---
+++ continuecondition.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+//continue statement loop program
+
+int main()
+{
+	int i,number,count=0;
+	
+	for(i=0;i<=10;i++){
+		printf("this is the loop iteration %d\n\n",i);
+		
+		printf("enter the positive number if you entered the negative number the loop will be terminated :\n");
+		scanf("%d",&number);
+		
+		if(number < 0)
+		{
+			continue;
+		}
+		count=count+number;
+	}
+	printf("the result is: %d",count);
+	return 0;
+}
